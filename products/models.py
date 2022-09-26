d479de26c483adf2203597b82d2547e6ddddbd3e
main <mehdi190gym@gmail.com>
2022-09-26 21:13:15 +0330

    first commit

---
+++ products/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Category(models.Model):
+    parent = models.ForeignKey('self', verbose_name=_('parent'),blank=True,null=True,on_delete=models.CASCADE)
+    title = models.CharField(_('title'),max_length=50)
+    description = models.TextField(_('description'),blank=True)
+    avatar = models.ImageField(_('avatar'),blank=True, upload_to='categories/')
+    is_enable = models.BooleanField(_('is enable'),default=True)
+    created_time = models.DateTimeField(_('created time'),auto_now_add=True)
+    update_time = models.DateTimeField(_('update time'),auto_now=True)
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+
+class Product(models.Model):
+    title = models.CharField(_('title'), max_length=50)
+    description = models.TextField(_('description'), blank=True)
+    avatar = models.ImageField(_('avatar'), blank=True, upload_to='products/')
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    categories = models.ManyToManyField('Category', verbose_name=_('categories'),blank=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    update_time = models.DateTimeField(_('update time'), auto_now=True)
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = _('product')
+        verbose_name_plural = _('products')
+
+
+class File(models.Model):
+    pass
